e4f3dc0e98e9049aeaaaaf7393ba29665e000d94
fran33397 <fran33397@gmail.com>
2023-02-28 14:06:59 -0600

    works 50%

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,18 @@
 import "./portfolio.scss"
 import PortfolioList from "../portfolioList/PortfolioList";
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
 
   const [selected,setSelected] = useState("featured")
+  const [data,setData] = useState([])
 
   const list=[
     {id: "featured",
@@ -22,6 +30,31 @@ export default function Portfolio() {
     {id: "branding",
     title: "Branding"}
   ];
+
+  useEffect(()=>{
+
+    switch(selected){
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "branding":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+
+  },[selected])
+
   return (
     <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
@@ -36,10 +69,15 @@ export default function Portfolio() {
         ))}
         </ul>
         <div className="container">
-          <div className="item">
-            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.IuWuNqMEJWHLUzfmfLkBcwHaEJ%26pid%3DApi&f=1&ipt=e352491f500c377a188c6012d0ba1a6f5117a7b006f2866f66db13c8b0a0952a&ipo=images" alt="" />
-            <h3>XML Reading</h3>
-          </div>
+          {data.map((d)=>(
+            <div className="item">
+              <img src={d.img} alt="" />
+              <h3>{d.title}</h3>
+            </div>
+            
+
+
+          ))}
         </div>
     </div>
   )
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,28 @@ import "./works.scss"
 export default function Works() {
   return (
     <div className="works" id="works">
-
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/globe.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate distinctio quod omnis rerum deleniti odit optio dignissimos perferendis voluptatum dolores quam magnam dicta ullam, totam a voluptatem! Iusto, similique. Magni?</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+            <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt="" />
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" />
+      <img src="assets/arrow.png" className="arrow right" alt="" />
     </div>
   )
 }
